32cfc796668c876f93c97db934d56d31c010a8b0
tshadz <thais.sombra@hotmail.com>
2021-07-26 18:27:35 -0300

    :package: Page de signUp

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState } from "react"
+
 const useForm = (initialState) => {
+
   const [input, setForm] = useState(initialState);
+
   const onChangeInput = (event) => {
     const { name, value } = event.target;
     setForm({ ...input, [name]: value });
-  };
+  }
+
   const cleanFields = () => {
     setForm(initialState);
-  };
+  }
+
   return { input, onChangeInput, cleanFields };
-};
+}
+
 export default useForm;
