53323d7ac1efc51638a331a337f65359a4ddeb6e
Izabella <bellacbrandao@gmail.com>
2021-07-29 15:59:26 -0300

    :art: fixando footer

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -5,6 +5,7 @@ import OrdersHistory from '../../components/OrdersHistory/OrdersHistory'
 import PersonalData from '../../components/PersonalData.js/PersonalData'
 import UserAdress from '../../components/UserAdress/UserAdress'
 import { goToLogin } from '../../routes/coordinator'
+import { ContainerProfile } from './styled'
 
 const Profile = ({setLogged, changeInfoHeader}) =>{
     const history = useHistory()
@@ -17,7 +18,7 @@ const Profile = ({setLogged, changeInfoHeader}) =>{
     }
     
     return(
-        <div>
+        <ContainerProfile>
             <PersonalData setLogged={setLogged} />
             <UserAdress />
             <p>Histórico de Pedidos</p>
@@ -33,7 +34,7 @@ const Profile = ({setLogged, changeInfoHeader}) =>{
             >
                 Logout
             </Button>
-        </div>
+        </ContainerProfile>
     )
 }
 export default Profile
