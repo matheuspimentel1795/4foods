c21622ead039a2a3f8b80e2042584ec85dadff13
danilo-ac <anthony.danilo@outlook.com.br>
2021-07-29 11:19:13 -0300

    :arrow_up: botao voltar na pag de busca

--- src/Pages/FeedSearch/FeedSearch.jsx
+++ src/Pages/FeedSearch/FeedSearch.jsx
@@ -4,10 +4,12 @@ import useProtectedPage from '../../hooks/useProtectedPage'
 import useRequestData from '../../hooks/useRequestData'
 import Loading from '../../components/Loading/Loading'
 import RestaurantCard from '../../components/RestaurantCard/RestaurantCard'
-import { Container, ContainerList } from './style'
+import { Container, ContainerList, ContainerRow } from './style'
 import { Input, OutlinedInput, Typography } from '@material-ui/core'
 import { useHistory } from 'react-router-dom'
 import useForm from '../../hooks/useForm'
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 const FeedSearch = ({ setLogged }) => {
     useProtectedPage()
@@ -33,11 +35,16 @@ const FeedSearch = ({ setLogged }) => {
 
     }) : input.search && !searchResult.length && <p>item não encontrado</p>
 
-    
+
     return (
         <Container>
-            
-            <Typography>Busca</Typography>
+            <ContainerRow>
+                <IconButton>
+                    <ArrowBackIosIcon onClick={() => history.goBack()} />
+                </IconButton>
+                <Typography>Busca</Typography>
+                <></>
+            </ContainerRow>
             <OutlinedInput
                 type="text"
                 onChange={onChangeInput}
@@ -46,7 +53,7 @@ const FeedSearch = ({ setLogged }) => {
                 placeholder={"Restaurante"}
             />
             <ContainerList>
-                {isLoading ? <Loading /> : renderResult ? renderResult : <>busque por nome de restaurante</>}
+                {isLoading ? <Loading /> : renderResult ? renderResult : <Typography>busque por nome de restaurante</Typography>}
             </ContainerList>
         </Container>
     )
