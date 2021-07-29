07f3f050468f1db55eedc506de66dcc3bcb32cbe
danilo-ac <anthony.danilo@outlook.com.br>
2021-07-29 10:54:27 -0300

    :package: funcionalidade de busca

--- src/Pages/FeedSearch/FeedSearch.jsx
+++ src/Pages/FeedSearch/FeedSearch.jsx
@@ -5,21 +5,48 @@ import useRequestData from '../../hooks/useRequestData'
 import Loading from '../../components/Loading/Loading'
 import RestaurantCard from '../../components/RestaurantCard/RestaurantCard'
 import { Container, ContainerList } from './style'
-import { OutlinedInput, Typography } from '@material-ui/core'
+import { Input, OutlinedInput, Typography } from '@material-ui/core'
 import { useHistory } from 'react-router-dom'
+import useForm from '../../hooks/useForm'
 
 const FeedSearch = ({ setLogged }) => {
     useProtectedPage()
     setLogged(true)
 
+    const [isLoading, setIsLoading] = useState(false)
+
+    const { input, onChangeInput } = useForm({
+        search: ""
+    })
+
+    const loadList = useRequestData({}, '/restaurants', setIsLoading)
+
     const history = useHistory()
 
+    const searchResult = input.search && loadList.restaurants?.filter((item) => {
+        return item.name.toLowerCase().includes(input.search.toLowerCase())
+
+    })
+
+    const renderResult = input.search && searchResult.length > 0 ? searchResult?.map((item) => {
+        return <RestaurantCard item={item} />
+
+    }) : input.search && !searchResult.length && <p>item não encontrado</p>
+
+    
     return (
         <Container>
+            
             <Typography>Busca</Typography>
-            <OutlinedInput />
+            <OutlinedInput
+                type="text"
+                onChange={onChangeInput}
+                value={input.search}
+                name={"search"}
+                placeholder={"Restaurante"}
+            />
             <ContainerList>
-                {/* {displayList ? displayList : <Loading/>} */}
+                {isLoading ? <Loading /> : renderResult ? renderResult : <>busque por nome de restaurante</>}
             </ContainerList>
         </Container>
     )
