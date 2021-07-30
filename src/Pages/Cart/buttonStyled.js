808519fe5c5e500782467c0deeb5f202fbcf3e2b
matheuspimentel1795 <matheuspimentel1795@gmail.com>
2021-07-30 15:37:09 -0300

    :package:estilizacao carrinho

---
+++ src/Pages/Cart/buttonStyled.js
@@ -0,0 +1,15 @@
+import Button from "@material-ui/core/Button";
+import {styled} from '@material-ui/core/styles'
+
+const MyButton = styled(Button)({
+    width: '90px',
+    height: '32px',
+    borderRadius: '0',
+    borderBottomRightRadius: '8px',
+    borderTopLeftRadius: '8px',
+    fontSize: '12px',
+    border: '1px solid #E02020',
+    color: '#E02020',
+  })
+
+  export default MyButton
